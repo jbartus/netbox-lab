dd5f2d0e6cebfc68f22ceb4e9530a2560ce37b6f
Jim Bartus <561971+jbartus@users.noreply.github.com>
2025-04-28 14:23:41 -0400

    first chip at enterprise

--- main.tf
+++ main.tf
@@ -16,6 +16,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   ip_protocol       = "tcp"
 }
 
+# resource "aws_vpc_security_group_ingress_rule" "allow_nbe_30k" {
+#   security_group_id = aws_security_group.netbox_lab.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 30000
+#   to_port           = 30000
+#   ip_protocol       = "tcp"
+# }
+
 resource "aws_iam_role" "lab_instance_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -37,15 +45,19 @@ resource "aws_iam_instance_profile" "lab_instance_profile" {
 }
 
 data "aws_ssm_parameter" "al2023_ami_arm64" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
 
 resource "aws_instance" "lab_instance" {
   ami                         = data.aws_ssm_parameter.al2023_ami_arm64.value
-  instance_type               = "t4g.xlarge"
+  instance_type               = "t3a.xlarge"
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.netbox_lab.id]
   user_data                   = file("${path.module}/userdata.sh")
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.lab_instance_profile.name
+
+  root_block_device {
+    volume_size = 100
+  }
 }
---
+++ nbe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+cd /root
+
+#curl -f "https://app.enterprise.netboxlabs.com/embedded/netbox-enterprise/stable" -H "Authorization: ${TOKEN}" -o netbox-enterprise-stable.tgz -s
+
+#tar zxvf netbox-enterprise-stable.tgz
+
+#./netbox-enterprise install --license license.yaml
