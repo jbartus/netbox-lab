a4634a582b3c976df843d5d0ff9a3f32121150a9
Jim Bartus <561971+jbartus@users.noreply.github.com>
2025-05-02 16:21:06 -0400

    first pass at diode

--- nbe.sh
+++ nbe.sh
@@ -9,3 +9,16 @@ cd /root
 #tar zxvf netbox-enterprise-stable.tgz
 
 #./netbox-enterprise install --license license.yaml
+
+dnf -y install docker
+systemctl enable --now docker
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/libexec/docker/cli-plugins/docker-compose
+chmod +x /usr/libexec/docker/cli-plugins/docker-compose
+
+cat > .env << 'EOF'
+DIODE_TO_NETBOX_API_KEY=
+NETBOX_TO_DIODE_API_KEY=
+DIODE_API_KEY=
+EOF
+
+#docker compose up -d
