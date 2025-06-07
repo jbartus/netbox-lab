9181025151e71f5645aa60b0e7de69d27d4695be
Jim Bartus <561971+jbartus@users.noreply.github.com>
2025-06-06 20:19:33 -0400

    8pm friday quality code

---
+++ ansible.sh.tpl
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+dnf -y install ansible
+
+cd /root
+
+cat << 'EOF' > ansible.cfg
+${ansible_cfg}
+EOF
+
+cat << 'EOF' > ansible_nb_inv.yaml
+${ansible_nb_inv_yaml}
+EOF
+
+NETBOX_API=https://${netbox_api}/
+
+echo "export NETBOX_API=$${NETBOX_API}" >> .bash_profile
+
+until [ "$(curl -o /dev/null -sk --max-time 2 -w '%%{http_code}' "$${NETBOX_API}")" -eq 200 ]; do
+    sleep 30
+done
+
+TOKEN=$(curl "$${NETBOX_API}/api/users/tokens/provision/" -H 'Content-Type: application/json' -d '{"username": "admin", "password": "${admin_password}"}' -sk | jq '.key' -r)
+
+echo "export NETBOX_TOKEN=$${TOKEN}" >> .bash_profile
---
+++ ansible.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "ansible" {
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_vpc_security_group_egress_rule" "ansible_allow_all_out" {
+  security_group_id = aws_security_group.ansible.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_instance" "ansible_instance" {
+  ami                    = data.aws_ssm_parameter.al2023_ami_arm64.value
+  instance_type          = "t4g.large"
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.ansible.id]
+  user_data = templatefile("${path.module}/ansible.sh.tpl", {
+    ansible_cfg         = file("${path.module}/ansible.cfg"),
+    ansible_nb_inv_yaml = file("${path.module}/ansible_nb_inv.yaml"),
+    netbox_api          = aws_instance.nbe_instance.private_ip
+    admin_password      = var.nbe_admin_password
+  })
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile.name
+}
+
+output "ansible_ssm_command" {
+  value = "aws ssm start-session --target ${aws_instance.ansible_instance.id}"
+}
