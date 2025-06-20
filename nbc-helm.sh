103c41eacd7efcf3a5f0e1ea5e0da3b4f84c7db0
Jim Bartus <561971+jbartus@users.noreply.github.com>
2025-06-20 09:44:02 -0400

    redis is stateless so no point in pulling it out of the cluster

--- nbc-helm.sh
+++ nbc-helm.sh
@@ -7,19 +7,9 @@ helm install netbox oci://ghcr.io/netbox-community/netbox-chart/netbox \
   --set postgresql.enabled=false \
   --set externalDatabase.host=$(terraform output -raw postgres_host) \
   --set externalDatabase.password=$(terraform output -raw postgres_password) \
-  --set valkey.enabled=false \
-  --set tasksDatabase.host=$(terraform output -raw redis_host) \
-  --set cachingDatabase.host=$(terraform output -raw redis_host)
-
-kubectl -n netbox create secret generic netbox-valkey \
-  --from-literal=cache_password="" \
-  --from-literal=task_password=""
 
 # admin password
 kubectl -n netbox get secrets netbox-superuser -o jsonpath="{.data.password}" | base64 --decode
 
-# port forward
-# kubectl -n netbox port-forward svc/netbox 8080:80
-
-# cleanup
-# helm -n netbox uninstall netbox
+echo # newline
+echo "kubectl -n netbox port-forward svc/netbox 8080:80"
