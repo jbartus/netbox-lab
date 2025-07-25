b41a3f77d05e18830fd49587b7e419bb45781574
Jim Bartus <561971+jbartus@users.noreply.github.com>
2025-07-25 09:49:49 -0400

    upstream dependency deadlock resolved: https://github.com/jbartus/netbox-lab/commit/9c0295deae2b2bd3e24939bb548c03686253eb1b

--- eks.tf
+++ eks.tf
@@ -1,13 +1,14 @@
 module "eks" {
-  source       = "terraform-aws-modules/eks/aws"
-  cluster_name = "lab-cluster"
-  vpc_id       = module.vpc.vpc_id
-  subnet_ids   = module.vpc.private_subnets
+  source             = "terraform-aws-modules/eks/aws"
+  name               = "lab-cluster"
+  kubernetes_version = "1.33"
+  vpc_id             = module.vpc.vpc_id
+  subnet_ids         = module.vpc.private_subnets
 
-  cluster_endpoint_public_access           = true
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
+  addons = {
     aws-ebs-csi-driver = {
       most_recent = true
     }
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
   source             = "terraform-aws-modules/vpc/aws"
-  version            = "5.21.0"
   name               = "terraform-vpc"
   cidr               = "10.0.0.0/16"
   azs                = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
