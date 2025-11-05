4c832ed16d1343488b58c6197de6b4c43fdcfdf6
Jim Bartus <561971+jbartus@users.noreply.github.com>
2025-11-05 12:35:01 -0500

    something broke upstream w/pg but at least its in the stable channel now

--- nbe-helm.sh
+++ nbe-helm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-helm install netbox-enterprise oci://registry.enterprise.netboxlabs.com/netbox-enterprise/beta/netbox-enterprise \
+helm install netbox-enterprise oci://registry.enterprise.netboxlabs.com/netbox-enterprise/stable/netbox-enterprise \
   --namespace netbox-enterprise \
   --create-namespace \
   --set global.license.id=$(grep nbe_token terraform.tfvars | awk '{print $3}' | sed 's/\"//g') \
-  --values nbe-values.yaml \
-  --version 1.11.5
+  --values nbe-values.yaml
 
-kubectl -n netbox-enterprise wait --for=condition=Ready pod -l app.kubernetes.io/name=netbox,app.kubernetes.io/component=netbox --timeout=10m
+kubectl -n netbox-enterprise wait \
+  --for=condition=Ready pod -l app.kubernetes.io/name=netbox,app.kubernetes.io/component=netbox \
+  --timeout=10m
 
 kubectl -n netbox-enterprise get secret netbox-enterprise-secret-config -o jsonpath="{.data.password}" | base64 --decode
 
