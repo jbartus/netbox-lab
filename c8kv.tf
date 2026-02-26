26efc9b13897c70b2ee05de22e9b06521d97f7ac
Jim Bartus <561971+jbartus@users.noreply.github.com>
2026-02-26 17:32:48 -0500

    best i can tell, cisco changed which amis to use and how to reference them

--- c8kv.tf
+++ c8kv.tf
@@ -28,27 +28,14 @@ resource "aws_vpc_security_group_ingress_rule" "c8kv_allow_snmp_in" {
   ip_protocol       = "udp"
 }
 
-data "aws_ami" "c8kv_byol" {
-  count  = var.enable_discovery ? 1 : 0
-  owners = ["aws-marketplace"]
-  filter {
-    name   = "name"
-    values = ["Cisco-C8K-*"]
-  }
-  filter {
-    name   = "product-code"
-    values = ["3ycwqehancx46bkpb3xkifiz5"]
-  }
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-  most_recent = true
+data "aws_ssm_parameter" "c8kv_byol" {
+  count = var.enable_discovery ? 1 : 0
+  name  = "/aws/service/marketplace/prod-rhyp4n745z2jk/latest"
 }
 
 resource "aws_instance" "c8kv_instance" {
   count                       = var.enable_discovery ? 1 : 0
-  ami                         = data.aws_ami.c8kv_byol[0].id
+  ami                         = data.aws_ssm_parameter.c8kv_byol[0].value
   instance_type               = "c5n.large"
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.c8kv[0].id]
